0f8e91d2f440a3d885b5b17a53820fc571b6be89
Joris van de Sande <joris@vandesande.link>
2021-01-30 12:08:25 +0100

    Added ArchiveClient to fetch documents from Yuki

--- README.md
+++ README.md
@@ -4,3 +4,29 @@ Yuki API Client
 An API client for the Yuki SOAP API endpoints.
 
 [![Build Status](https://travis-ci.org/jorisvandesande/yuki-api-client.svg?branch=master)](https://travis-ci.org/jorisvandesande/yuki-api-client)
+
+At the moment only a couple of API calls are implemented:
+
+* Archive
+  * DocumentBinaryData()
+  * DocumentsInFolder()
+  * SearchDocuments()
+* Contact
+  * SearchContacts()
+* Sales
+  * ProcessSalesInvoices()
+
+Usage
+=====
+```php
+use JVDS\YukiClient\ContactClient;
+
+$client = new ContactClient('your-yuki-access-key');
+$client->searchContacts();
+```
+
+Official Yuki documentation
+===========================
+
+Documentation for all Yuki endpoints can be found at: 
+  https://support.yuki.nl/nl/support/solutions/articles/11000062989-yuki-api-koppeling
---
+++ src/ArchiveClient.php
@@ -0,0 +1,119 @@
+<?php
+declare(strict_types=1);
+
+namespace JVDS\YukiClient;
+
+use JVDS\YukiClient\Exception\Exception;
+use JVDS\YukiClient\Exception\UnexpectedTypeException;
+use JVDS\YukiClient\Soap\ClientFactory;
+use SimpleXMLElement;
+
+class ArchiveClient extends Client
+{
+    private const WSDL = 'https://api.yukiworks.nl/ws/Archive.asmx?wsdl';
+
+    public function __construct(
+        string $accessKey,
+        array $soapClientOptions = [],
+        ClientFactory $factory = null,
+        string $wsdl = self::WSDL
+    ) {
+        parent::__construct($accessKey, $wsdl, $soapClientOptions, $factory);
+    }
+
+    /**
+     * @param int $folderID
+     * @param int $startRecord
+     * @param int $numberOfRecords
+     * @param string $sortOrder CreatedDesc or CreatedAsc or ModifiedDesc or ModifiedAsc or DocumentDateDesc or DocumentDateAsc or ContactNameAsc or ContactNameDesc
+     * @param string $startDate date in Y-m-d\TH:i:s format, or 0001-01-01 to ignore the date
+     * @param string $endDate date in Y-m-d\TH:i:s format, or 0001-01-01 to ignore the date
+     * @return SimpleXMLElement[]
+     * @throws Exception
+     * @throws UnexpectedTypeException
+     *
+     * @link https://support.yuki.nl/nl/support/solutions/articles/11000062998-archive-webservice
+     * @Link https://api.yukiworks.nl/ws/archive.asmx?op=DocumentsInFolder
+     */
+    public function documentsInFolder(
+        int $folderID,
+        int $startRecord = 1,
+        int $numberOfRecords = 100,
+        string $sortOrder = 'ModifiedDesc',
+        string $startDate = '0001-01-01',
+        string $endDate = '0001-01-01'
+    ): array {
+        $arguments = compact('folderID', 'startRecord', 'numberOfRecords', 'sortOrder', 'startDate', 'endDate');
+
+        $response = $this->call('DocumentsInFolder', $arguments);
+
+        $xmlResponse = $response->DocumentsInFolderResult->any ?? null;
+
+        if (is_string($xmlResponse)) {
+            return iterator_to_array((new SimpleXMLElement($xmlResponse)), false);
+        }
+
+        UnexpectedTypeException::fromValue($xmlResponse, 'string');
+    }
+
+    /**
+     * @param string $searchText
+     * @param string $searchOption All or Creator or Contact or Subject or Tag or Type
+     * @param int $folderID
+     * @param int $startRecord
+     * @param int $numberOfRecords
+     * @param string $sortOrder CreatedDesc or CreatedAsc or ModifiedDesc or ModifiedAsc or DocumentDateDesc or DocumentDateAsc or ContactNameAsc or ContactNameDesc
+     * @param string $startDate date in Y-m-d\TH:i:s format, or 0001-01-01 to ignore the date
+     * @param string $endDate date in Y-m-d\TH:i:s format, or 0001-01-01 to ignore the date
+     * @return SimpleXMLElement[]
+     * @throws Exception
+     * @throws UnexpectedTypeException
+     *
+     * @link https://support.yuki.nl/nl/support/solutions/articles/11000062998-archive-webservice
+     * @Link https://api.yukiworks.nl/ws/archive.asmx?op=SearchDocuments
+     */
+    public function searchDocuments(
+        string $searchText = '',
+        string $searchOption = 'All',
+        int $folderID = -1,
+        int $startRecord = 1,
+        int $numberOfRecords = 100,
+        string $sortOrder = 'ModifiedDesc',
+        string $startDate = '0001-01-01',
+        string $endDate = '0001-01-01'
+    ): array {
+        $arguments = compact('searchText', 'ssearchOption', 'folderID', 'startRecord', 'numberOfRecords', 'sortOrder', 'startDate', 'endDate');
+
+        $response = $this->call('SearchDocuments', $arguments);
+
+        $xmlResponse = $response->SearchDocumentsResult->any ?? null;
+
+        if (is_string($xmlResponse)) {
+            return iterator_to_array((new SimpleXMLElement($xmlResponse)), false);
+        }
+
+        UnexpectedTypeException::fromValue($xmlResponse, 'string');
+    }
+
+    /**
+     * @param string $documentID
+     * @return SimpleXMLElement
+     *
+     * @link https://support.yuki.nl/nl/support/solutions/articles/11000062998-archive-webservice
+     * @Link https://api.yukiworks.nl/ws/archive.asmx?op=DocumentBinaryData
+     */
+    public function documentBinaryData(string $documentID): SimpleXMLElement
+    {
+        $arguments = compact('documentID');
+
+        $response = $this->call('DocumentBinaryData', $arguments);
+
+        $xmlResponse = $response->DocumentBinaryDataResult->any ?? null;
+
+        if (is_string($xmlResponse)) {
+            return new SimpleXMLElement($xmlResponse);
+        }
+
+        UnexpectedTypeException::fromValue($xmlResponse, 'string');
+    }
+}
