8a21657aeed48a0a1c64a50ff2be664c188b175c
dragomirt <dragomirt22@gmail.com>
2021-02-09 17:12:49 +0200

    Adds Aministrations client

---
+++ src/AdministrationsClient.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace JVDS\YukiClient;
+
+use JVDS\YukiClient\Exception\Exception;
+use JVDS\YukiClient\Exception\UnexpectedTypeException;
+use JVDS\YukiClient\Soap\ClientFactory;
+use SimpleXMLElement;
+
+class AdministrationsClient extends Client
+{
+    private const WSDL = 'https://api.yukiworks.nl/ws/Accounting.asmx?wsdl';
+
+    protected const SESSION_ID_ARG = 'sessionID';
+
+    public function __construct(
+        string $accessKey,
+        array $soapClientOptions = [],
+        ClientFactory $factory = null,
+        string $wsdl = self::WSDL
+    ) {
+        parent::__construct($accessKey, $wsdl, $soapClientOptions, $factory);
+    }
+
+
+    public function Administrations(
+    ): SimpleXMLElement {
+        $response = $this->call('Administrations', []);
+
+        $xmlResponse = $response->AdministrationsResult->any ?? null;
+
+        if (is_string($xmlResponse)) {
+            return new SimpleXMLElement($xmlResponse);
+        }
+
+        UnexpectedTypeException::fromValue($xmlResponse, 'string');
+    }
+}
